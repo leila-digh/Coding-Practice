6cf45d36b9e5c5e04d5becc46bffa5743b190cca
leila-digh <leiladabakuyo@cmail.carleton.ca>
2025-05-11 16:44:05 -0400

    emordnilap etadpu

--- 125 - Valid Palindrome - LeetCode/Solution.java
+++ 125 - Valid Palindrome - LeetCode/Solution.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String sCleaned = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        String sBack = "";
-        for(int i = sCleaned.length()-1; i >= 0; i--){
-            sBack = sBack + sCleaned.charAt(i);
-        }
-        // System.out.println(sBack);
-        // System.out.println(sCleaned);
-
-        if(sBack.equals(sCleaned)){
+        cleanString(s);
+        String notS = reverseString(s);
+        
+        if(notS.equals(s)){
             return true;
         }
         return false;
     }
+
+    public void cleanString(String s){
+        s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    }
+
+    public String reverseString(String s){
+        String result = "";
+         for(int i = s.length()-1; i >= 0; i--){
+            result = result + s.charAt(i);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution sol = new Solution();
         String tests[] = {" ", "civic", "radar", "kayak", "madam", "refer", "tattarrattat", "A man, a plan, a canal: Panama", "race a car", "0"};
